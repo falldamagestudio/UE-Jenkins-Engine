4a9c52d0e056bc0078d3f09b828acbbe34758999
Mikael Kalms <mikael@kalms.org>
2020-12-27 15:44:13 +0100

    Use persistent volume for workspace

--- build-engine-linux.groovy
+++ build-engine-linux.groovy
@@ -1,17 +1,25 @@
-
 pipeline {
   agent {
-    kubernetes { yaml """
+    kubernetes { 
+
+      // The given PersistentVolumeClaim will be mounted where the workspace folder typically is located.
+	    // The PVC must have been created beforehand, outside of Jenkins.
+	    // The PVC ensures that a persistent disk of a given size has been created.
+	    // It enables incremental builds.
+      workspaceVolume persistentVolumeClaimWorkspaceVolume(claimName: 'build-engine-linux', readOnly: false)
+
+      yaml """
 metadata:
   labels:
     app: jenkins-agent
+
 spec:
 
   # Schedule this pod onto a node in the jenkins agent node pool
   nodeSelector:
     jenkins-agent-node-pool: "true"
 
-  # Ensure this pod is not scheduled onto a node that already has another jenkins agent pod
+  # Ensure this pod is not scheduled onto a node that already has another jenkins agent pod on it
   affinity:
     podAntiAffinity:
       requiredDuringSchedulingIgnoredDuringExecution:
@@ -35,22 +43,23 @@ spec:
 """
     }
   }
+
   stages {
+
     stage('Fetch Git repo dependencies') {
       steps {
-
         sh "cd UE/Engine/Build/BatchFiles/Linux && ./Setup.sh"
-
       }
     }
     
-    stage('Build Engine (Linux)') {
-    
-      steps {
+//    stage('Build Engine (Linux)') {
+//    
+//      steps {
+//
+//        sh "./Scripts/Linux/BuildEngine.sh"
+//
+//      }
+//    }
 
-        sh "./Scripts/Linux/BuildEngine.sh"
-
-      }
-    }
   }
 }
