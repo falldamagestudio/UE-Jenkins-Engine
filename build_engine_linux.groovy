045f085165a98d85204627e60c7dff8b4efa77d7
Mikael Kalms <mikael@kalms.org>
2021-01-05 22:26:16 +0100

    Include tag in image parameter

--- build_engine_linux.groovy
+++ build_engine_linux.groovy
@@ -45,7 +45,7 @@ spec:
   containers:
   - name: jnlp
   - name: ue-jenkins-buildtools-linux
-    image: ${UE_JENKINS_BUILDTOOLS_LINUX_IMAGE}:${UE_JENKINS_BUILDTOOLS_LINUX_IMAGE_TAG}
+    image: ${UE_JENKINS_BUILDTOOLS_LINUX_IMAGE}
     # Add dummy command to prevent container from immediately exiting upon launch
     command:
     - cat
