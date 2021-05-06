279a02bff6b4e91de59a787c77037a9aac6dcf0d
Mikael Kalms <mikael@kalms.org>
2021-05-06 18:12:21 +0200

    Run GitDependencies in force-overwrite mode first

--- build_engine_linux_git_docker.groovy
+++ build_engine_linux_git_docker.groovy
@@ -24,6 +24,13 @@ pipeline {
       steps {
           sh """
               cd UE
+
+              # Run GitDependencies once, in a mode where diffs are force-overwritten
+              # GitDepedencies will be invoked again by Setup.sh, but then in 'prompt for any changes'
+              #   mode; Jenkins will hang when any user input is requested; by running it once in
+              #   force-overwrite mode first we ensure that there will be no conflicts during the second run
+              ./Engine/Build/BatchFiles/Linux/GitDependencies.sh --force
+
               ./Setup.sh
             """
       }
