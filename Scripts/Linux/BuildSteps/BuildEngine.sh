6ee0b5e97e2c288a1ef61ba55f9cef6f338163ba
Mikael Kalms <mikael@kalms.org>
2021-05-07 15:41:10 +0200

    Commented out core of most build steps

--- Scripts/Linux/BuildSteps/BuildEngine.sh
+++ Scripts/Linux/BuildSteps/BuildEngine.sh
@@ -4,4 +4,4 @@ ScriptRoot=$(dirname "$0")
 
 UELocation="${ScriptRoot}/../../../UE"
 
-"${UELocation}/Engine/Build/BatchFiles/RunUAT.sh" BuildGraph -Script="Engine/Build/InstalledEngineBuild.xml" -Target="Make Installed Build Linux" -set:HostPlatformOnly=true -set:WithServer=true -set:WithClient=true -set:WithDDC=false
+#"${UELocation}/Engine/Build/BatchFiles/RunUAT.sh" BuildGraph -Script="Engine/Build/InstalledEngineBuild.xml" -Target="Make Installed Build Linux" -set:HostPlatformOnly=true -set:WithServer=true -set:WithClient=true -set:WithDDC=false
