11e05da713b0035cd50de7f3dff963b991b47825
Mikael Kalms <mikael@kalms.org>
2021-05-07 15:24:52 +0200

    Added UploadUE mechanism + longtail 0.1.14

---
+++ Scripts/Linux/UploadUE.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+CloudStorageBucket=$1
+BuildId="$2"
+
+ScriptRoot=$(dirname "$0")
+
+UELocation="${ScriptRoot}/../../UE"
+EngineBuildLocation="${UELocation}/LocalBuilds/Engine/Linux"
+EngineBuildId="engine-${BuildId}-linux"
+
+"${ScriptRoot}/Upsync-EngineBuild.sh" "$EngineBuildLocation" "$CloudStorageBucket" "$EngineBuildId"
---
+++ Scripts/Linux/Upsync-EngineBuild.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+EngineBuildLocation="$1"
+CloudStorageBucket="$2"
+EngineBuildId="$3"
+
+ScriptRoot=$(dirname "$0")
+
+LongtailLocation="${ScriptRoot}/longtail-linux-x64"
+VersionIndexURI="gs://${CloudStorageBucket}/engine-linux/index/${EngineBuildId}.lvi"
+StorageURI="gs://${CloudStorageBucket}/engine-linux/storage"
+
+"${LongtailLocation}" \
+    upsync \
+    --source-path \
+    $EngineBuildLocation \
+    --target-path \
+    $VersionIndexURI \
+    --storage-uri \
+    $StorageURI
